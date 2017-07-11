9275e55ae6578f6b63b1eabee958df9c62d47279
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2017-07-12 02:31:29 +1200

    Module 2 App

--- SpellCheck/SpellCheck/HomePage.xaml.cs
+++ SpellCheck/SpellCheck/HomePage.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace SpellCheck
 {
-    public partial class MainPage : TabbedPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
     {
-        public MainPage()
+        public HomePage()
         {
             InitializeComponent();
         }
--- SpellCheck/SpellCheck/Search.xaml.cs
+++ SpellCheck/SpellCheck/Search.xaml.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace SpellCheck
 {
-    public partial class MainPage : TabbedPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Search : ContentPage
     {
-        public MainPage()
+        public Search()
         {
             InitializeComponent();
         }
+
+
     }
 }
