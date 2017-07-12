73cfe4d4311ef8b9de46e14af120f4f7fa7c44aa
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2017-07-12 12:04:32 +1200

    Started implementing GET request

--- SpellCheck/SpellCheck/History.xaml.cs
+++ SpellCheck/SpellCheck/History.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,17 +12,15 @@ namespace SpellCheck
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class History : ContentPage
     {
-        int count = 0;
         public History()
         {
             InitializeComponent();
         }
 
-        private void getHistory(object sender, EventArgs e)
+        async void getHistory(object sender, EventArgs e)
         {
-            count++;
-
-            ((Button)sender).Text = count.ToString();
+            List<skmuspellchecktable> spellCheckRows = await AzureManager.AzureManagerInstance.getAllRows();
+            WordList.ItemsSource = spellCheckRows;
         }
     }
 }
