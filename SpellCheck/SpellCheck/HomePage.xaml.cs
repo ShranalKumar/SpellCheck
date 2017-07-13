264884e3baae3707b1bfb0441c7a6c2899e2a9ea
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2017-07-13 13:56:47 +1200

    Working on searchbar

--- SpellCheck/SpellCheck/HomePage.xaml.cs
+++ SpellCheck/SpellCheck/HomePage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,38 @@ namespace SpellCheck
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        private string _searchedText;
+        public string SearchedText
+        {
+            get { return _searchedText; }
+            set { _searchedText = value; }
+        }
+
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        public ICommand MakeRequest { get; set; }
+
+        async void MakeRequestExecute()
+        {
+            var client = new HttpClient();
+            var queryString = searchBar.Text;
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "233d11b1fba54760a24a2243eff1f6cc");
+
+            var uri = "https://api.cognitive.microsoft.com/bing/v5.0/spellcheck/?" + queryString;
+
+            HttpResponseMessage response;
+
+            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);                
+            }
+            correct.Text = response.ToString();
         }
     }
 }
